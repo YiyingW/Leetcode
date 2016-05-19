c61d0b9ba9ef25e06b61f4309ce0c79cdd37e9fd
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 17:49:54 -0400

    [64. Minimum Path Sum][Accepted]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.py
+++ 64-Minimum-Path-Sum/solution.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def minPathSum(self, grid):
         """
@@ -8,12 +7,15 @@ class Solution(object):
         nrow = len(grid)
         ncol = len(grid[0])
         path = {}
-        for i in range(0, ncol):
-            path[(0, i)] = grid[0][i]
-        for i in range(0, nrow):
-            path[(i, 0)] = grid[i][0]
+        path[(0,0)] = grid[0][0]
 
         for i in range(1, ncol):
-            for j in range(1, nrow):
-                path[(i, j)] = min(path[(i-1,j)], path[(i, j-1)], path[(i-1, j-1)])
+            path[(0, i)] = path[(0,i-1)] + grid[0][i]
+        for i in range(1, nrow):
+            path[(i, 0)] = path[(i-1, 0)] + grid[i][0]
+
+        for i in range(1, nrow):
+            for j in range(1, ncol):
+                path[(i, j)] = min(path[(i-1,j)], path[(i, j-1)]) + grid[i][j]
         return path[(nrow-1, ncol-1)]
+
