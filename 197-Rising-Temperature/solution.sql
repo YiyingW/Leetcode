93301eafa51da549acea8a9d69b18e4557326c8c
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 14:06:01 -0400

    [197. Rising Temperature][Runtime Error]committed by LeetCode Extension

--- 197-Rising-Temperature/solution.sql
+++ 197-Rising-Temperature/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT W2.Id
 FROM Weather AS W1, Weather AS W2
-WHERE DATEDIFF(W1.Date, W2,Date)=1 AND W1.Temperature < W2.Temperature;
+WHERE julianday(W1.Date) - julianday(W2.Date) = -1 AND W1.Temperature < W2.Temperature;
