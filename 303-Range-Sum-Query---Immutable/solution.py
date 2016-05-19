a4497231a0d9d3237981f934119c1b17bb3b6153
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-18 23:18:24 -0400

    [303. Range Sum Query - Immutable][Time Limit Exceeded]committed by LeetCode Extension

--- 303-Range-Sum-Query---Immutable/solution.py
+++ 303-Range-Sum-Query---Immutable/solution.py
@@ -1,16 +1,25 @@
-class NumArray {
-public:
-    NumArray(vector<int> &nums) {
-        
-    }
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.nums = nums
 
-    int sumRange(int i, int j) {
-        
-    }
-};
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        result = 0
+        for k in range(i, j+1):
+            result += self.nums[k]
+        return result
 
 
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.sumRange(1, 2);
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
