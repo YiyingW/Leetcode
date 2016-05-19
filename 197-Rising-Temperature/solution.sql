05084c9a6e2f76d89ce8377f16e191eb39395fb3
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 14:11:29 -0400

    [197. Rising Temperature][Accepted]committed by LeetCode Extension

--- 197-Rising-Temperature/solution.sql
+++ 197-Rising-Temperature/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT W2.Id
 FROM Weather AS W1, Weather AS W2
-WHERE julianday(W1.Date) - julianday(W2.Date) = -1 AND W1.Temperature < W2.Temperature;
+WHERE DATEDIFF(W2.Date, W1.Date)=1 AND W1.Temperature < W2.Temperature;
