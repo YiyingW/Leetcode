cd0223444011db7c7f6171ffe4273a71860bfde2
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 17:36:57 -0400

    [64. Minimum Path Sum][Runtime Error]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.py
+++ 64-Minimum-Path-Sum/solution.py
@@ -1,3 +1,4 @@
+
 class Solution(object):
     def minPathSum(self, grid):
         """
@@ -12,14 +13,7 @@ class Solution(object):
         for i in range(0, nrow):
             path[(i, 0)] = grid[i][0]
 
-        if ncol > 1 and nrow > 1:
-            for i in range(1, ncol):
-                for j in range(1, nrow):
-                    path[(i, j)] = min(path[(i-1,j)], path[(i, j-1)], path[(i-1, j-1)])
-            return path[(nrow, ncol)]
-        elif ncol > 1:
-            return path[(0, ncol)]
-        elif nrow > 1:
-            return path[(nrow, 0)]
-        else:
-            return path[(0,0)]
+        for i in range(1, ncol):
+            for j in range(1, nrow):
+                path[(i, j)] = min(path[(i-1,j)], path[(i, j-1)], path[(i-1, j-1)])
+        return path[(nrow-1, ncol-1)]
