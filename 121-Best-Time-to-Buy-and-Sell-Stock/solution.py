a6528cdba3a287d3f95e196670a42108772ec72d
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 16:01:47 -0400

    [121. Best Time to Buy and Sell Stock][Accepted]committed by LeetCode Extension

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.py
@@ -4,15 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        min_history = float("inf")
+        self.prices = prices
+        min_stock = float("inf")
         max_result = 0
-        for i in range(0, len(prices)):
-            current = prices[i]
-            min_stock = min(min_history, current)
+        for current in self.prices:
+            min_stock = min(min_stock, current)
             new = current - min_stock
-            max_result = max(self.maxProfit(prices[0:i]), new)
-        
+            max_result = max(max_result,new)
         return max_result
+
+       
+
             
             
         
