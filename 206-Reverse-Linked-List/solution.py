355415e2656ead9415ee43434f514a694fabea5c
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-19 21:09:12 -0400

    [206. Reverse Linked List][Runtime Error]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        currentNode = head
+        previousNode = None
+        while (currentNode.next != None):
+            nextNode = currentNode.next
+            currentNode.next = previousNode
+            previousNode = currentNode
+            currentNode = nextNode
+
+        currentNode.next = previousNode
+        return currentNode
