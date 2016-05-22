7aca633fd2afa7cab6ca039325f34e4fd8553f66
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-22 18:48:35 -0400

    [181. Employees Earning More Than Their Managers][Accepted]committed by LeetCode Extension

--- 181-Employees-Earning-More-Than-Their-Managers/solution.sql
+++ 181-Employees-Earning-More-Than-Their-Managers/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT E1.Name
+SELECT E1.Name AS Employee
 FROM Employee AS E1, Employee AS E2
-WHERE E1.ManagerId == E2.Id AND E1.Salary > E2.Salary;
+WHERE E1.ManagerId = E2.Id AND E1.Salary > E2.Salary;
