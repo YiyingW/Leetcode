cd1a96de92ca23c220b12f9b8418d6eee6ec250d
Yiying Wang <YiyingW@users.noreply.github.com>
2016-05-23 16:53:13 -0400

    [125. Valid Palindrome][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 125-Valid-Palindrome/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = self.simplifyString(s)
+        if s == '': return True
+        for i in range(0, len(s)/2):
+            if s[i] != s[len(s)-i-1]:
+                return False
+        return True
+        
+    def simplifyString(self, s):
+        new = ''
+        for letter in s:
+            if ord(letter)>=ord('a') and ord(letter)<=ord('z'):
+                new+=letter
+            elif ord(letter)>=ord('A') and ord(letter)<=ord('Z'):
+                chara = chr(ord('a')+ord(letter)-ord('A'))
+                new+=chara
+        return new
+        
