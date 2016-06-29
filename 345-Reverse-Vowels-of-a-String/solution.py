5b94e322feb166b7072e1daf521d710c8d321a56
Yiying Wang <YiyingW@users.noreply.github.com>
2016-06-29 01:03:35 -0400

    [345. Reverse Vowels of a String][Accepted]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.py
+++ 345-Reverse-Vowels-of-a-String/solution.py
@@ -1,23 +1,24 @@
 class Solution(object):
-	def reverseVowels(self, s):
-		string = [l for l in s]
-		i = 0
-		j = len(string) - 1
-		vowels = ['a','e','i','o','u','A','E','I','O','U']
-		while (i < j):
-			if string[i] in vowels and string[j] in vowels:
-				string[i], string[j] = string[j], string[i]
-				i += 1
-				j -= 1
-			elif string[i] in vowels and string[j] not in vowels:
-				j -= 1
-			elif string[i] not in vowels and string[j] in vowels:
-				i += 1
-			else:
-				i += 1
-				j -= 1
+  def reverseVowels(self, s):
+    """
+    : tyte s : str
+    : rtype : str
+    """
+    vowels = []
+    s_list = list(s)
+    count = len(s_list) - 1
+    for i in range(0, len(s_list)):
+      if s_list[i] in ['a','e','i','o','u','A','E','I','O','U']:
 
+        while (s_list[count] not in ['a','e','i','o','u','A','E','I','O','U']):
+          count = count - 1
+        s_list[count],s_list[i] = s_list[i],s_list[count]
 
+        count = count - 1
 
+      if i >= count:
+        break;
 
-		return "".join(string)
+    str = "".join(s_list)
+
+    return str
